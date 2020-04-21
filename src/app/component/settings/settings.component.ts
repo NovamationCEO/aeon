cf442317e073c63cba4bfedbd31f887c76795519
Chris Young <chris.young@blitzpredict.io>
2020-04-21 13:14:32 -0600

    Players Count Bar added, new colors

--- src/app/component/settings/settings.component.ts
+++ src/app/component/settings/settings.component.ts
@@ -9,14 +9,30 @@ import { LayoutStoreService } from "../../store/layout-store.service";
 export class SettingsComponent implements OnInit {
 
   status: boolean;
+  deckType: string;
+  hidePlayers: boolean;
 
   constructor(private layout: LayoutStoreService) { }
 
   ngOnInit() {
-    this.layout.settingsOpen.subscribe(newStatus => this.status = newStatus)
+    this.layout.settingsOpen.subscribe(newStatus => this.status = newStatus);
+    this.deckType = "2";
+    this.hidePlayers = true;
   }
 
   toggle() {
     this.layout.toggleSettings();
+  }
+
+  setDeckType(newValue) {
+      if (newValue == this.deckType) {
+          return
+      }
+
+      this.deckType = newValue;
+  }
+
+  toggleShowPlayers() {
+      this.hidePlayers = !this.hidePlayers;
   }
 }
